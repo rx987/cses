208a4f13764debd05498e2deed0bbb7c2e7af8ab
rx987 <cxrseq9876@gmail.com>
2024-02-10 01:02:49 +0530

    src: solved number spiral

---
+++ introductory_problems/number_spiral.py
@@ -0,0 +1,19 @@
+def main():
+    test = int(input())
+
+    for _ in range(test):
+        y, x = map(int, input().split())
+        if y > x:
+            if y % 2:
+                ans = (y - 1) ** 2 + x
+            else:
+                ans = y**2 - x + 1
+        else:
+            if x % 2:
+                ans = x**2 - y + 1
+            else:
+                ans = (x - 1) ** 2 + y
+        print(ans)
+
+
+main()
