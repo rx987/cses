e593bf9cfa7a97de5edb6ee8fcb42b15938744f4
rx987 <cxrseq9876@gmail.com>
2024-02-10 00:59:44 +0530

    src: solved permutations.py

---
+++ introductory_problems/permutations.py
@@ -0,0 +1,21 @@
+def main():
+    n = int(input())
+    if n == 1:
+        print(1)
+    elif n <= 3:
+        print("NO SOLUTION")
+    else:
+        # print all the odd numbers in decreasing order, then all even numbers in decreasing order
+        even = []
+        odd = []
+
+        for i in range(n, 0, -1):
+            if i % 2 == 0:
+                even.append(i)
+            else:
+                odd.append(i)
+        print(*(odd + even))
+
+
+if __name__ == "__main__":
+    main()
